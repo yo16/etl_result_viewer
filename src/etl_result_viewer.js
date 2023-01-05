3a2b0c4d1bfb7f617503b486d423fa6063f88c03
yo16 <xxyo1xx@gmail.com>
2023-01-05 23:52:20 +0900

    first source

---
+++ src/etl_result_viewer.js
@@ -0,0 +1,17 @@
+`use strict`
+/*
+etl_result_viewer
+Copyright (c) 2023 yo16
+Released under the MIT license.
+*/
+
+class EtlResult {
+    constructor(dom_id) {
+        this.dom_id = dom_id;
+        console.log(this.dom_id)
+    }
+
+    // データを元に描画
+    data(original_data) {
+    }
+}
