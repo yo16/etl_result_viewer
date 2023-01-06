d28afeb5551a437572030c8d8fed95ae67991a29
yo16 <xxyo1xx@gmail.com>
2023-01-07 03:30:47 +0900

    add text

--- src/etl_result_viewer.js
+++ src/etl_result_viewer.js
@@ -9,7 +9,6 @@ Released under the MIT license.
 const SVG_WIDTH = 700;
 const SVG_HEIGHT = 400;
 const PADDING = [10, 10, 10, 10];   // top, right, bottom, left
-const PROCESS_NAME_COLUMN_WIDTH = 200;
 
 // 日時関連
 // ヘッダー
@@ -18,6 +17,10 @@ const TIME_LINE_HEIGHT = 50;
 const HOUR_WIDTH = 100;
 // 0時以外の開始位置
 const HOUR_BAR_START = 35;
+
+// プロセス関連
+// ヘッダー
+const PROCESS_NAME_COLUMN_WIDTH = 200;
 
 class EtlResult {
     constructor(dom_id)
@@ -37,6 +40,9 @@ class EtlResult {
 
         // 日時関連のものを描く
         this.draw_time_line(svg, original_data);
+
+        // プロセス定義関連のものを描く
+        this.draw_process_info(svg, original_data);
     }
 
     // 日時関連の描画
@@ -73,7 +79,6 @@ class EtlResult {
         let diff_hours = (max_dt.getTime() - min_dt.getTime())/(60*60*1000);
 
         let cur_dt = min_dt;
-        console.log(cur_dt);
         let dts = [];
         for (let i=0; i<diff_hours; i++) {
             cur_dt.setHours(cur_dt.getHours()+1)
@@ -85,7 +90,7 @@ class EtlResult {
             .append('g')
             .attr('class', 'timeline_bar')
             .attr('stroke-width', 1)
-            .attr('stroke', '#CCCCCC')
+            .attr('stroke', '#CCC')
         ;
         for (let i=0; i<dts.length; i++) {
             const x = HOUR_WIDTH*i;
@@ -115,7 +120,7 @@ class EtlResult {
             .append('g')
             .attr('class', 'timeline_text')
             .attr('stroke-width', 1)
-            .attr('fill', '#999999')
+            .attr('fill', '#999')
         ;
         for (let i=0; i<dts.length; i++) {
             const x = HOUR_WIDTH*i;
@@ -141,5 +146,95 @@ class EtlResult {
 
     }
 
+    // プロセス定義関連のものを描く
+    draw_process_info(top_svg, original_data)
+    {
+        // ここでのsvg
+        let svg = top_svg
+            .append('svg')
+            .attr('id', 'svg_process')
+            .attr('x', PADDING[3])
+            .attr('y', PADDING[0]+TIME_LINE_HEIGHT)
+            .attr('width', SVG_WIDTH-(PADDING[1]+PADDING[3]))
+            .attr('height', SVG_HEIGHT-(PADDING[0]+TIME_LINE_HEIGHT+PADDING[2]))
+        ;
+        
+        // アイコン
+        svg
+            .append('defs')
+            .append('path')
+            .attr('id', 'folder')
+            .attr('d', 'M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H298.5c-17 0-33.3-6.7-45.3-18.7L226.7 50.7c-12-12-28.3-18.7-45.3-18.7H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z')
+            .attr('transform', 'scale(0.025)')
+            .attr('fill', '#999')
+        ;
 
+        // データに階層番号(depth, 0, 1, ...)を付ける
+        let data = {};   // idをキーに取り出せるようにする
+        for (let i=0; i<original_data.length; i++) {
+            console.assert('id' in original_data[i], '"id" is not defined.');
+            data[original_data[i].id] = original_data[i];
+            if (!('parent_id' in original_data[i])) {
+                data[original_data[i].id]['parent_id'] = null;
+            }
+        }
+        let top_key = [];
+        Object.keys(data).forEach( k => {
+            if (data[k].parent_id === null) {
+                data[k]['depth'] = 0;
+                top_key.push(data[k].id);
+            }
+        });
+        // 自分が親になっている子に、自分のdepth+1を設定
+        let set_depth = (d) => {
+            Object.keys(data).forEach( k => {
+                if (data[k].parent_id === d.id ) {
+                    data[k]['depth'] = d.depth + 1;
+                    set_depth(data[k]);
+                }
+            })
+        };
+        for ( let i=0; i<top_key.length; i++ ) {
+            set_depth(data[top_key[i]]);
+        }
+
+
+        // rect
+        svg
+            .append('rect')
+            .attr('class', 'rect_top')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', PROCESS_NAME_COLUMN_WIDTH)
+            .attr('height', SVG_HEIGHT-(PADDING[0]+TIME_LINE_HEIGHT+PADDING[2]))
+            .attr('fill', '#EEE')
+            .attr('opacity', '70%')
+        ;
+        // プロセスを描く
+        function draw_one_process(svg, d, i)
+        {
+            let cur_g = svg
+                .append('g')
+                .attr('class', 'one_process')
+            ;
+            cur_g
+                .append('use')
+                .attr('xlink:href', '#folder')
+                .attr('x', 25 + d.depth * 20)
+                .attr('y', 38 + i * 33)
+            ;
+            cur_g
+                .append('text')
+                .text(d.title)
+                .attr('x', 45 + d.depth * 20)
+                .attr('y', 50 + i * 33)
+                .attr('font-size', 12)
+                .attr('fill', '#333')
+            ;
+
+        }
+        Object.keys(data).forEach( (k,i) => {
+            draw_one_process(svg, data[k], i)
+        });
+    }
 }
