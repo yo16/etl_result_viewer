e58a5e1c084369c6029c4073e173f96cc0ff8f3b
yo16 <xxyo1xx@gmail.com>
2023-02-19 01:20:37 +0900

    using node

---
+++ node/src/index.js
@@ -0,0 +1,14 @@
+`use strict`
+/*
+etl_result_viewer
+Copyright (c) 2023 yo16
+Released under the MIT license.
+*/
+
+function main(dom_id, data) {
+    console.log({dom_id});
+    console.log({data});
+}
+
+module.exports.etl_result_viewer = main;
+
